90bef363eb8a131657a537c36adc5f7ca99732f3
Sarah <sarah.kichler.estudante@petrobras.com.br>
2024-04-17 15:35:10 -0300

    Subindo arquivos

--- api.js
+++ api.js
@@ -1,23 +1,19 @@
-// requires
+// 17/04/2024 - vamos criar dois novos endpoints
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
-//imports
 const mysql_config = require('./imp/mysql_config');
 const functions = require('./imp/functions');
 
-//variáveis para disponibilidade e para versionamento
 const api_availability = true;
 const api_version = '1.0.0';
 
-//iniciar sevidor
 const app = express();
 app.listen(3000, () => {
     console.log('API em execução.');
 })
 
-//verificar a disponobilidade da api
 app.use((req, res, next) => {
     if (api_availability) {
         next();
@@ -26,16 +22,14 @@ app.use((req, res, next) => {
     }
 })
 
-//conxão com mysql
 const connection = mysql.createConnection(mysql_config);
 
-//cors
 app.use(cors());
 
 //rotas
 //rota inicial (rota de entrada)
 app.get('/', (req, res) => {
-    res.json(functions.response('sucesso', 'API rodando', 0, null))
+    res.json(functions.response('sucesso', 'API rodando', 0, null));
 })
 
 //endpoint
@@ -50,7 +44,44 @@ app.get('/tasks', (req, res) => {
     })
 })
 
-//tratar o erro da rota
+//rota para fazer uma consulta de task por id
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query('SELECT * FROM tasks WHERE id  = ?', [id], (err, rows) => {
+        if (!err) {
+            if (rows.length > 0) {
+                res.json(functions.response('Sucesso', 'Pesquisa bem sucedida', rows.length, rows));
+            } else {
+                res.json(functions.response('Atenção', 'Não foi encontrada a task celecionada', 0, null));
+            }
+        } else {
+            res.json(functions.response('Erro', err.message, 0, null));
+        }
+    })
+})
+
+//rota para atualizar o status da task pelo id selecionado
+app.put('/tasks/:id/status/:status', (req, res) => {
+    const id = req.params.id;
+    const status = req.params.status;
+    // primeiro se informa o status pq é a coluna, depois o id 
+    connection.query('UPDATE tasks SET status = ? WHERE  id = ?', [status, id], (err, rows) => {
+        if (!err) {
+            if (rows.affectedRows > 0) {
+                res.json(functions.response('Sucesso', 'Sucesso na alteração de status', rows.affectedRows, null));
+            } else {
+                res.json(functions.response('Alerta vermelho', 'Task não encontrada', 0, null));
+            }
+        } else {
+            res.json(functions.response('Erro', err.message, 0, null));
+        }
+    })
+})
+
+
+
+
+
 app.use((req, res) => {
     res.json(functions.response('Atenção', 'Rota não encontrada', 0, null));
 })
